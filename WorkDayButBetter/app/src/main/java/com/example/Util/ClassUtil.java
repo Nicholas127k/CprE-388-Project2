96ae60fe9134907735132d7354c38d2ad6b3de6c
Nick127k <Nick127k@iastate.edu>
2024-11-14 16:13:21 -0600

    Classes.java

--- WorkDayButBetter/app/src/main/java/com/example/Util/ClassUtil.java
+++ WorkDayButBetter/app/src/main/java/com/example/Util/ClassUtil.java
@@ -1,4 +1,19 @@
 package com.example.Util;
 
+import android.content.Context;
+
+import com.example.model.Classes;
+
+import java.util.Arrays;
+import java.util.Random;
+
 public class ClassUtil {
+    private static final String TAG = "ClassUtil";
+
+    public static Classes getRandom(Context context) {
+        Classes classes = new Classes();
+
+
+        return classes;
+    }
 }
