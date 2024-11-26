4865801ce8d9fb467f8b90e440278c0d543c1373
NathanTPerkins <108106121+NathanTPerkins@users.noreply.github.com>
2024-11-26 13:32:55 -0600

    added changes to class.java

--- WorkDayButBetter/app/src/main/java/com/example/data_classes/Class.java
+++ WorkDayButBetter/app/src/main/java/com/example/data_classes/Class.java
@@ -2,6 +2,7 @@ package com.example.data_classes;
 
 import com.example.utilities.ClassSectionTimeRange;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -10,6 +11,8 @@ public class Class {
     public static final String FIELD_CODE = "code";
     public static final String FIELD_NAME = "name";
     public static final String FIELD_DESCRIPTION = "description";
+
+    public static final int SUCCESS = 0;
 
     /**
      *
@@ -54,13 +57,17 @@ public class Class {
         this.code = -1;
         this.name = null;
         this.description = null;
+        this.classMembers = new ArrayList<>();
+        this.signUpQueue = new ArrayList<>();
     }
 
-    public Class(String department, long code, String name, String description) {
+    public Class(String department, long code, String name, String description, List<User> classMembers, List<User> signUpQueue) {
         this.department = department;
         this.name = name;
         this.description = description;
         this.code = code;
+        this.classMembers = classMembers;
+        this.signUpQueue = signUpQueue;
     }
 
     /**
@@ -86,6 +93,14 @@ public class Class {
         return this.description;
     }
 
+    public List<User> getClassMembers() {
+        return this.classMembers;
+    }
+
+    public List<User> getSignUpQueue() {
+        return this.signUpQueue;
+    }
+
     /**
      *
      *
@@ -109,4 +124,51 @@ public class Class {
         this.description = description;
     }
 
+    public void setClassMembers(List<User> classMembers) {
+        this.classMembers = classMembers;
+    }
+
+    public void setSignUpQueue(List<User> queue){
+        this.signUpQueue = queue;
+    }
+
+
+    public static final int ERROR_MEMBER_ALREADY_EXISTS_IN_SIGN_UP_QUEUE = 1;
+    public int addMemberToSignUpQueue(User user){
+        boolean memberExists = false;
+        for(int i = 0; i < this.signUpQueue.size(); ++i){
+            User member = this.signUpQueue.get(i);
+            if(member.getId_() == user.getId_()){
+                memberExists = true;
+            }
+        }
+
+        if(memberExists){
+            return ERROR_MEMBER_ALREADY_EXISTS_IN_SIGN_UP_QUEUE;
+        }
+
+        this.signUpQueue.add(user);
+        return SUCCESS;
+    }
+
+
+    public static final int ERROR_MEMBER_DOES_NOT_EXIST_IN_SIGN_UP_QUEUE = 2;
+    public int removeMemberFromSignUpQueue(User user){
+        boolean memberExists = false;
+        int memberIndex = 0;
+        for(int i = 0; i < this.signUpQueue.size(); ++i){
+            User member = this.signUpQueue.get(i);
+            if(member.getId_() == user.getId_()){
+                memberExists = true;
+                memberIndex = i;
+            }
+        }
+
+        if(!memberExists){
+            return ERROR_MEMBER_DOES_NOT_EXIST_IN_SIGN_UP_QUEUE;
+        }
+
+        this.signUpQueue.remove(memberIndex);
+        return SUCCESS;
+    }
 }
