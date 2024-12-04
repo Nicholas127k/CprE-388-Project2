987e29b7a851ad6332292242b55a5754bf6f2e7c
Nick127k <Nick127k@iastate.edu>
2024-12-03 20:55:54 -0600

    Me and Chat GPT have been Cooking

---
+++ WorkDayButBetter/app/src/main/java/com/example/adapter/TabAdapter.java
@@ -0,0 +1,39 @@
+package com.example.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.workdaybutbetter.AdvisorMainScreen;
+import com.example.workdaybutbetter.AdvisorMainScreenFragment;
+import com.example.workdaybutbetter.ControlFragment;
+import com.example.workdaybutbetter.CreateClassFragment;
+
+public class TabAdapter extends FragmentStateAdapter {
+
+    public TabAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new AdvisorMainScreenFragment();
+            case 1:
+                return new CreateClassFragment();
+            case 2:
+                return new ControlFragment();
+            default:
+                return new AdvisorMainScreenFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;  // Total number of tabs
+    }
+}
+
---
+++ WorkDayButBetter/app/src/main/java/com/example/workdaybutbetter/AdvisorMainScreenFragment.java
@@ -0,0 +1,18 @@
+package com.example.workdaybutbetter;
+
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AdvisorMainScreenFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_advisor_main_screen, container, false);
+    }
+}
