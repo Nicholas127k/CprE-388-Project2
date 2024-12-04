5d14c48cf497f8ae2efb79e0f78197adb072a655
Nick127k <Nick127k@iastate.edu>
2024-12-03 20:55:54 -0600

    Me and Chat GPT have been Cooking

---
+++ WorkDayButBetter/app/src/main/java/com/example/workdaybutbetter/AdvisorMainScreen.java
@@ -0,0 +1,41 @@
+package com.example.workdaybutbetter;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.adapter.TabAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class AdvisorMainScreen extends AppCompatActivity {
+
+    private ViewPager2 viewPager;
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advisor_main_screen);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        TabAdapter tabAdapter = new TabAdapter(this);
+        viewPager.setAdapter(tabAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("Main");
+                    break;
+                case 1:
+                    tab.setText("Create Class");
+                    break;
+                case 2:
+                    tab.setText("Control");
+                    break;
+            }
+        }).attach();
+    }
+}
